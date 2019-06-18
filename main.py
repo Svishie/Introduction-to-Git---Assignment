8b7609ab1c6a811a7e3bcff8bcee2b74c626ba1a
Anders Kvalvik Kvernberg <Anders.Kvalvik.Kvernberg@kvaerner.com>
2019-06-18 08:53:58 +0200

    Implemented function that prints even numbers

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def even_numbers(start=0, end=100):
+    print("Printing even numbers between {} and {}".format(start, end))
+    for i in range(start, end):
+        if i % 2 == 0:
+            print(i)
+
 def hello_world():
     print("Hello World")
 
