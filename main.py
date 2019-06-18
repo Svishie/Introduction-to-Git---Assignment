6b8e59442e560afec7e7b579fda1aeff74d8da63
Anders Kvalvik Kvernberg <Anders.Kvalvik.Kvernberg@kvaerner.com>
2019-06-18 08:50:23 +0200

    Implemented a function to print odd numbers. Wrote a function that prints all odd numbers between two given numbers.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def odd_numbers(start=0, end=100):
+    print("Printing odd numbers from {} to {}".format(start, end))
+    for i in range(start, end):
+        if i % 2 != 0:
+            print(i)
+
 def hello_world():
     print("Hello World")
 
