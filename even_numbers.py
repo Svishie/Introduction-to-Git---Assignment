e5b86cf57d759bce214e3519a03b6a6a4d754f44
Anders Kvalvik Kvernberg <Anders.Kvalvik.Kvernberg@kvaerner.com>
2019-06-18 09:03:02 +0200

    Implemented a function that prints even numbers The function prints all even numbers between two given parameters, start and end. The parameters defaults to 0 and 100.

---
+++ even_numbers.py
@@ -0,0 +1,5 @@
+def even_numbers(start=0, end=100):
+    print("Printing even numbers between {} and {}".format(start, end))
+    for i in range(start, end):
+        if i % 2 == 0:
+            print i
