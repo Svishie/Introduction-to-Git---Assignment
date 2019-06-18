03c57b84b183ccbaa99cf1966e312ee28c071d01
Anders Kvalvik Kvernberg <Anders.Kvalvik.Kvernberg@kvaerner.com>
2019-06-18 09:15:57 +0200

    Added is_palindrome Function is_palindrome checks if a given string is a palindrome

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def hello_world():
     print("Hello World")
 
+def is_palindrome(palindrome):
+    reversed = palindrome[::-1]
+
+    if(palindrome == reversed):
+        print("{} is a palindrome".format(palindrome))
+    else:
+        print("{} is not a palindrome".format(palindrome))
+
 if __name__ == "__main__":
     hello_world()
+    is_palindrome("asa")
+    is_palindrome("asb")
